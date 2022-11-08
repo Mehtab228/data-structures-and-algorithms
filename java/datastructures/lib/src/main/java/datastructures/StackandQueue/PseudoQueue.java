c254906dcfebabad533d5b5359bf5d7ca57532bc
Mehtab Riar <mehtabriar228@hotmail.com>
2022-11-07 17:16:25 -0800

    finished code need to test

--- java/datastructures/lib/src/main/java/datastructures/StackandQueue/PseudoQueue.java
+++ java/datastructures/lib/src/main/java/datastructures/StackandQueue/PseudoQueue.java
@@ -1,4 +1,25 @@
 package datastructures.StackandQueue;
 
-public class PseudoQueue {
-}
+public class PseudoQueue<T> {
+  public Stack<T> front;
+  public Stack<T> back;
+  public PseudoQueue() {
+    this.front = new Stack<>();
+    this.back = new Stack<>();
+  }
+
+  public void enqueue(T value) {
+    back.push(value);
+  }
+
+  public T dequeue() {
+    if(front.isEmpty()){
+      while(!back.isEmpty()){
+        front.push(back.pop());
+      }
+    }
+    return front.pop();
+  }
+
+  }
+
