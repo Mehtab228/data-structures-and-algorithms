9e142ffb73801200c436abc916335cd7fa6ad608
Mehtab Riar <mehtabriar228@hotmail.com>
2022-11-08 18:52:53 -0800

    finished challenge

---
+++ java/datastructures/lib/src/main/java/datastructures/StackandQueue/Animal.java
@@ -0,0 +1,12 @@
+package datastructures.StackandQueue;
+
+public class Animal {
+  String species;
+  public Animal(String species) {
+    this.species = species;
+  }
+
+  public String getSpecies() {
+    return species;
+  }
+}
