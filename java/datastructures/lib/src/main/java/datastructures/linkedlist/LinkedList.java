27d92d5da40360ee63a0b96038f0cd9be0629b22
Mehtab Riar <mehtabriar228@hotmail.com>
2022-11-30 18:01:09 -0800

    finished challenge

--- java/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
+++ java/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
@@ -1,112 +1,157 @@
 package datastructures.linkedlist;
 
-public class LinkedList<I extends Number> {
+import java.util.HashMap;
+
+public class LinkedList<T> {
   public Node head = null;
-  private int sizeOfList = 0;
-  public LinkedList(){}
+  private int size = 0;
 
-public void insert(String value) {
-  // 3 steps, 1. Create new node with the value, set the previous head to be next, set the head to be the value passed in (newHead)
-  Node newHead = new Node(value);
-  newHead.next = head;
-  head = newHead;
-  sizeOfList++;
-}
-
-public boolean includes(String value) {
-  Node current = head;
-  while (current.next != null) {
-    if (current.next.equals(value))
-      return true;
-      current = current.next;
+  // Code Challenge 05
+  public void insert(T value) {
+    Node<T> newHeadNode = new Node<>((String) value);
+    newHeadNode.next = head;
+    head = newHeadNode;
+    size++;
   }
-  return false;
-}
 
-  @Override
-  public String toString()
-    {
-      StringBuilder newStr = new StringBuilder();
-      Node current = head;
-      while(current != null) {
-        newStr.append("{" + " " + current.value+ " " + "} ->");
-        current = current.next;
-      }
-        newStr.append("NULL");
-      return  newStr.toString();
+  public boolean includes(T value) {
+    Node<T> current = head;
+    while(current != null) {
+      if(current.value.equals(value))
+        return true;
+      current = current.next;
     }
+    return false;
+  }
+  @Override
+  public String toString() {
+    String output = "";
+    Node<T> current = head;
+    while(current != null) {
+      output += "{ " + current.value + " } -> ";
+      current = current.next;
+    }
+    return output + "NULL";
+  }
 
-    public void append(String value) {
-      Node current = head;
+  // Code Challenge 06
+
+  public void append(T value) {
+    Node<T> newTailNode = new Node<>((String) value);
+    if(head == null) {
+      newTailNode.next = null;
+      head = newTailNode;
+    } else {
+      Node<T> current = head;
       while(current.next != null) {
         current = current.next;
       }
-      Node newNode = new Node(value);
-      current.next = newNode;
+      current.next = newTailNode;
+      newTailNode.next = null;
     }
+    size++;
+  }
 
-    public void insertBefore(String searchValue, String addValue) {
-      Node current = head;
-      while(current != null) {
-        if (current.next.value == searchValue) {
-          Node newNode = new Node(addValue);
-          newNode.next = current.next;
-          current.next = newNode;
-          return;
-        }
+  public void insertBefore(T value, T newValue) {
+    if (head == null) {
+      throw new IllegalArgumentException(value + " not found in list");
+    }
+    if(head.value == value) {
+      insert(newValue);
+      return;
+    }
+    Node<T> current = head;
+    Node<T> insertNode = new Node<>((String) newValue);
+    while(current.next != null) {
+      if(current.next.value.equals(value)) {
+        insertNode.next = current.next;
+        current.next = insertNode;
+        size++;
+        break;
+      } else
         current = current.next;
-      }
-      System.out.println("Search Value not found");
     }
+    if(current.next == null)
+      throw new IllegalArgumentException(value + " not found in list");
+  }
 
-  public void insertAfter(String searchValue, String addValue){
+  public void insertAfter(T value, T newValue) {
+    if (head == null) {
+      throw new IllegalArgumentException(value + " not found in list");
+    }
+    Node<T> current = head;
+    Node<T> insertNode = new Node<>((String) newValue);
+    while(current != null) {
+      if(current.value.equals(value)) {
+        insertNode.next = current.next;
+        current.next = insertNode;
+        size++;
+        break;
+      }
+      current = current.next;
+    }
+    if(current == null)
+      throw new IllegalArgumentException(value + " not found in list");
+  }
+
+  public void delete(T value) {
+    if (head == null) {
+      throw new IllegalArgumentException(value + " not found in list");
+    }
+    if (head.value.equals(value)) {
+      head = head.next;
+      size--;
+      return;
+    }
     Node current = head;
-    while(current != null){
-      if (current.value == searchValue){
-        Node newNode = new Node(addValue);
-        newNode.next = current.next;
-        current.next = newNode;
+    while(current.next != null) {
+      if(current.next.value.equals(value)) {
+        current.next = (current.next.next);
+        size--;
         return;
       }
       current = current.next;
     }
-    System.out.println("Search value not found");
+    if(current.value.equals(value))
+      current = null;
+    else
+      throw new IllegalArgumentException(value + " not found in list");
   }
 
-  public String kthFromEnd(int k){
-    if(k < 0) throw new IllegalArgumentException("Input can not be negative.");
-    int i = 0;
-    Node front = head;
-    Node rear = null;
-    while (front != null){
-      if(i-k > 0){
-        rear = rear.next;
-      } else if (i-k == 0) {
-        rear = head;
-      }
-      front = front.next;
-      i++;
-    }
-    if(i-k < 0) throw new IllegalArgumentException("Input exceeds linked list length.");
-    return rear.value;
-  }
-
-  public LinkedList<Integer> zipLists(LinkedList<Number> list1, LinkedList<Integer> list2) {
-    if(list1.sizeOfList < 1)
-      return list2;
-    if(list2.sizeOfList < 1)
-      return list2;
-    LinkedList<Integer> output = new LinkedList<>();
+  public T kthFromEnd(int k) {
+    if(head == null || k < 0)
+      throw new IllegalArgumentException("position " + k + " out of bounds");
+    HashMap<Integer, T> valueMap = new HashMap<>();
+    Node<T> current;
+    current = head;
     int position = 0;
-    while(position < list1.sizeOfList || position < list2.sizeOfList) {
-      if(position < list1.sizeOfList)
-        output.append(list1.kthFromEnd(list1.sizeOfList - position - 1));
-      if(position < list2.sizeOfList)
-        output.append(list2.kthFromEnd(list2.sizeOfList - position - 1));
-      position++;
+    while(current != null) {
+      valueMap.put(position++, (T) current.value);
+      current = current.next;
     }
-    return output;
+    if(k >= valueMap.size())
+      throw new IllegalArgumentException("position " + k + " out of bounds");
+    return valueMap.get((valueMap.size() - 1) - k);
   }
 
+  public T kthFromEndWithSize(int k) {
+    int targetPosition = (size - 1) - k;
+    if(k < 0 || targetPosition < 0 || targetPosition > size)
+      throw new IllegalArgumentException("position " + k + " out of bounds");
+    Node<T> current;
+    current = head;
+    int position = 0;
+    while(current != null) {
+      if(position == targetPosition) {
+        return (T) current.value;
+      }
+      position++;
+      current = current.next;
+    }
+    throw new IllegalArgumentException("position " + k + " out of bounds");
+  }
 
+  public int size() {
+    return size;
+  }
 }
